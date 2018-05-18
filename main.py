aa8035e80043d9f4a0de2f39d00bb45c4cf3397b
lemoer <git@irrelefant.net>
2018-05-18 12:55:52 +0200

    update

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import binascii
 import struct
 from crc32c import crc, crc_update, crc_finalize, CRC_INIT
 from collections import namedtuple
+import sys
 
 # Flag definitions
 # remote flags
@@ -23,7 +24,17 @@ BATADV_TT_CLIENT_TEMP	 = (1 << 11)
 
 SYNCED_FLAGS = BATADV_TT_CLIENT_ISOLA | BATADV_TT_CLIENT_WIFI
 
-record="   33:33:00:00:00:01   -1 [.W..] (  4) 5e:1c:3b:6c:a1:3b (  4) (0xce93f3f0)"
+# records= \
+# """ * 33:33:ff:de:ee:3e   -1 [....] ( 67) 82:39:e1:58:ca:cb ( 71) (0x3ca83da5)
+#    01:00:5e:00:00:01   -1 [....] ( 67) 82:39:e1:58:ca:cb ( 71) (0x3ca83da5)
+#  * 64:66:b3:de:ee:3e    0 [....] ( 67) 82:39:e1:58:ca:cb ( 71) (0xeb2fdd7a)
+#  * 64:66:b3:de:ee:3e   -1 [....] ( 67) 82:39:e1:58:ca:cb ( 71) (0x3ca83da5)
+#    33:33:00:00:00:01   -1 [....] ( 67) 82:39:e1:58:ca:cb ( 71) (0x3ca83da5)"""
+
+with open(sys.argv[1]) as f:
+    lines = f.read().split('\n')
+    lines.remove('')
+    records = '\n'.join(lines)
 
 TTGlobalEntry = namedtuple('TTGlobalEntry', 'client vid flags originator crc')
 
@@ -40,7 +51,6 @@ def read_tt_global(line):
     )
 
 def parse_mac(mac_str):
-    print(binascii.unhexlify(mac_str.replace(':', '')))
     return binascii.unhexlify(mac_str.replace(':', ''))
 
 def get_flag_repr(flag_str):
@@ -57,7 +67,7 @@ def get_flag_repr(flag_str):
 
     return repr
 
-def tt_global_crc(entry):
+def tt_global_crc_entry(entry):
     flags = get_flag_repr(entry.flags)
     if flags & BATADV_TT_CLIENT_ROAM:
         return None
@@ -68,18 +78,88 @@ def tt_global_crc(entry):
     tmp_vid = struct.pack('!h', entry.vid) # equivalent to htons()
     c = crc_update(CRC_INIT, tmp_vid)
 
-    tmp_flags = struct.pack('h', flags & SYNCED_FLAGS)
+    tmp_flags = struct.pack('H', flags & SYNCED_FLAGS)
     c = crc_update(c, tmp_flags)
 
-    tmp_client = parse_mac(entry.client) # TODO: reverse byte order?
+    tmp_client = parse_mac(entry.client)
     c = crc_update(c, tmp_client)
+    print(tmp_client)
 
     return crc_finalize(c)
 
+def tt_global_crc(records, orig, vid):
+    # orig as a str
+    c = 0
+    official_crc = None
 
-entry = read_tt_global(record)
-print(entry)
-print(entry.client)
-print(parse_mac(entry.client))
-print(get_flag_repr(entry.flags))
-print(tt_global_crc(entry))
+    for record in records.split('\n'):
+        entry = read_tt_global(record)
+
+        if entry.originator != orig:
+            continue
+
+        if entry.vid != vid:
+            continue
+
+        official_crc = int(entry.crc, 16)
+
+        # print(entry)
+        # print(entry.client)
+        # print(parse_mac(entry.client))
+        # print(get_flag_repr(entry.flags))
+
+        tmp_crc = tt_global_crc_entry(entry)
+
+        if tmp_crc is None:
+            # client is roaming or temp
+            continue
+
+        c ^= tmp_crc
+
+    #print("0x%x" % c)
+    return c, official_crc
+
+def unique(l):
+    return list(set(l))
+
+def get_originators(records):
+    originators = []
+
+    for record in records.split('\n'):
+        entry = read_tt_global(record)
+
+        originators += [entry.originator]
+        originators = unique(originators)
+
+    return originators
+
+def get_vids(records, orig):
+    vids = []
+
+    for record in records.split('\n'):
+        entry = read_tt_global(record)
+
+        if entry.originator != orig:
+            continue
+
+        vids += [entry.vid]
+        vids = unique(vids)
+
+    return vids
+
+#for orig in [get_originators(records)[0]]:
+for orig in get_originators(records):
+    if orig != "22:02:c6:74:f8:c3":
+        continue
+    for vid in get_vids(records, orig):
+        locally_calculated, official = tt_global_crc(records, orig, vid)
+        if locally_calculated != official:
+            print(orig, "%4d" % vid, "fail", "0x%x" % locally_calculated)
+            #print("0x%x" % official, "0x%x" % locally_calculated)
+        else:
+            print(orig, "%4d" % vid, "ok", "0x%x" % locally_calculated)
+
+# print()
+# print(get_vids(records, '82:39:e1:58:ca:cb'))
+# print("0x%x" % tt_global_crc(records, "82:39:e1:58:ca:cb", -1))
+# print("0x%x" % tt_global_crc(records, "82:39:e1:58:ca:cb", 0))
